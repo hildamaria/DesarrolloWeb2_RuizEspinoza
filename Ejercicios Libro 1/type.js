55e2773409ee36574a66cf31071153d843704fcc
hildamaria <hildamaria0609@hotmail.com>
2019-08-22 23:25:03 -0600

    Anotaciones de Tipo

--- Ejercicios Libro 1/type.js
+++ Ejercicios Libro 1/type.js
@@ -1,5 +1,5 @@
 function area(shape, width, height) {
     var area = width * height;
-    return "I,m a " + shape + "with an area of " + area + "cm squared.";
+    return "I,m a " + shape + " with an area of " + area + "cm squared.";
 }
 document.body.innerHTML = area("rectangule", 30, 15);
