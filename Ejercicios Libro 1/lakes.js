dcd24012a1247d2a2e54ee4262b8df39d5e4b909
hildamaria <hildamaria0609@hotmail.com>
2019-08-24 22:18:08 -0600

    Haciendo la Propiedades de la Interfaz Opcionales

--- Ejercicios Libro 1/lakes.js
+++ Ejercicios Libro 1/lakes.js
@@ -8,8 +8,18 @@ var firstlake = {
 };
 var secondLake = {
     name: 'Superior',
+    depth: 406.3,
     length: 616,
     area: 82100,
     isFreshwater: true,
     countries: ['Canada', 'United States']
+};
+var thirdLake = {
+    name: 'Baikal',
+    depth: 1637,
+    length: 636,
+    area: 31500,
+    isFreshwater: true,
+    countries: ['Russia'],
+    frozen: ['January', 'February', 'March', 'April', 'May']
 };
--- Ejercicios Libro 1/lakes.ts
+++ Ejercicios Libro 1/lakes.ts
@@ -4,7 +4,8 @@ interface Lakes{
     length: number,
     depth: number,
     isFreshwater: boolean,
-    countries: string[]
+    countries: string[],
+    frozen?: string[];
 }
 
 let firstlake: Lakes = {
@@ -18,10 +19,20 @@ let firstlake: Lakes = {
 
 let secondLake: Lakes = {
     name: 'Superior',
+    depth: 406.3,
     length: 616,
     area: 82100,
     isFreshwater: true,
     countries: ['Canada', 'United States']
-    }
+}
 
-    
+let thirdLake: Lakes = {
+    name: 'Baikal',
+    depth: 1637,
+    length: 636,
+    area: 31500,
+    isFreshwater: true,
+    countries: ['Russia'],
+    frozen: ['January', 'February', 'March', 'April', 'May']
+}
+
