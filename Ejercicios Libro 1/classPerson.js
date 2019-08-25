ea5aae045852ba36cadcfb22be55a8c40f7822d0
hildamaria <hildamaria0609@hotmail.com>
2019-08-24 23:26:12 -0600

    Usando el Modificador Protegido

--- Ejercicios Libro 1/classPerson.js
+++ Ejercicios Libro 1/classPerson.js
@@ -12,8 +12,9 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 var Person = /** @class */ (function () {
-    function Person(theName) {
+    function Person(theName, theAge) {
         this.name = theName;
+        this.age = theAge;
     }
     Person.prototype.introduceSelf = function () {
         console.log("Hi, I'm " + this.name + "!");
@@ -29,16 +30,24 @@ personA.introduceSelf();
 //HERENCIA
 var Friend = /** @class */ (function (_super) {
     __extends(Friend, _super);
-    function Friend(name, yearsKnown) {
-        var _this = _super.call(this, name) || this;
+    function Friend(name, age, yearsKnown) {
+        var _this = _super.call(this, name, age) || this;
         _this.yearsKnown = yearsKnown;
         return _this;
     }
     Friend.prototype.timeKnown = function () {
         console.log("We have been friends for " + this.yearsKnown + " years.");
     };
+    Friend.prototype.friendsSince = function () {
+        var firstAge = this.age - this.yearsKnown;
+        console.log("We have been friends since I was " + firstAge + " years old.");
+    };
     return Friend;
 }(Person));
 var friendA = new Friend("Jacob", 6);
 friendA.introduceSelf();
 friendA.timeKnown();
+var friendB = new Friend("William", 19, 8);
+friendB.introduceSelf();
+friendB.timeKnown();
+friendB.friendsSince();
--- Ejercicios Libro 1/classPerson.ts
+++ Ejercicios Libro 1/classPerson.ts
@@ -1,7 +1,10 @@
 class Person {
     private name: string;
-    constructor(theName: string){
+    protected age: number;
+
+    protected constructor(theName: string, theAge: number){
         this.name = theName;
+        this.age = theAge;
     }
     introduceSelf(){
         console.log("Hi, I'm " + this.name + "!");
@@ -19,15 +22,24 @@ personA.introduceSelf();
 //HERENCIA
 class Friend extends Person{
     yearsKnown: number;
-    constructor(name: string, yearsKnown: number){
-        super(name);
+    constructor(name: string, age: number, yearsKnown: number){
+        super(name, age);
         this.yearsKnown = yearsKnown;
     }
     timeKnown(){
         console.log("We have been friends for " + this.yearsKnown + " years.")
+    }
+    friendsSince(){
+        let firstAge = this.age - this.yearsKnown;
+        console.log(`We have been friends since I was ${firstAge} years old.`)
     }
 }
 
 let friendA = new Friend("Jacob",6);
 friendA.introduceSelf();
 friendA.timeKnown();
+
+let friendB = new Friend("William",19,8);
+friendB.introduceSelf();
+friendB.timeKnown();
+friendB.friendsSince();
