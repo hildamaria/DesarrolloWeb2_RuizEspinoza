41a6c9d7f90133c4806aba821e8f1616a7663351
hildamaria <hildamaria0609@hotmail.com>
2019-08-30 13:07:08 -0600

    Promesas en paralelo

--- Ejercicios Libro 2/promesas.js
+++ Ejercicios Libro 2/promesas.js
@@ -60,3 +60,28 @@ Promise.reject(new Error('something bad happened'))
     .then(function (res) {
     console.log(res); // 123
 });
+//PROMESAS EN PARALELO
+function loadItem(id) {
+    return new Promise(function (resolve) {
+        console.log('loading item', id);
+        setTimeout(function () {
+            resolve({ id: id });
+        }, 1000);
+    });
+}
+var item1, item2;
+loadItem(1)
+    .then(function (res) {
+    item1 = res;
+    return loadItem(2);
+})
+    .then(function (res) {
+    item2 = res;
+    console.log('done');
+});
+//Paralelo
+Promise.all([loadItem(1), loadItem(2)])
+    .then(function (res) {
+    item1.item2 = res[0];
+    console.log('done');
+});
--- Ejercicios Libro 2/promesas.ts
+++ Ejercicios Libro 2/promesas.ts
@@ -66,3 +66,31 @@ Promise.reject(new Error('something bad happened'))
 .then((res) => {
     console.log(res); // 123
 })
+
+//PROMESAS EN PARALELO
+function loadItem(id: number): Promise<{id: number}>{
+    return new Promise((resolve)=>{
+        console.log('loading item', id);
+        setTimeout(() => {
+            resolve({id:id});
+        }, 1000);
+    });
+}
+
+let item1, item2;
+loadItem(1)
+.then((res) => {
+    item1 = res;
+    return loadItem(2);
+})
+.then((res) => {
+    item2 = res;
+    console.log('done')
+});
+
+//Paralelo
+Promise.all([loadItem(1),loadItem(2)])
+.then((res) => {
+    [item1.item2] = res;
+    console.log('done')
+});
