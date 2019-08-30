ed8087b1214adef60d5e8be585795c011c4fdbb2
hildamaria <hildamaria0609@hotmail.com>
2019-08-30 00:13:49 -0600

    Fast Arrow

--- Ejercicios Libro 2/fatArrow.js
+++ Ejercicios Libro 2/fatArrow.js
@@ -1,3 +1,16 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var inc = function (x) { return x + 1; };
 function Person(age) {
     this.age = age;
@@ -44,3 +57,27 @@ var Person = /** @class */ (function () {
 var person = new Person(1);
 setTimeout(person.growOld, 1000);
 setTimeout(function () { console.log(person.age); }, 2000);
+var Adder = /** @class */ (function () {
+    function Adder() {
+        var _this = this;
+        // This function is now safe to pass around
+        this.add = function (b) {
+            return _this.a + b;
+        };
+    }
+    return Adder;
+}());
+var ExtendedAdder = /** @class */ (function (_super) {
+    __extends(ExtendedAdder, _super);
+    function ExtendedAdder() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        // Create a copy of parent before creating our own
+        _this.superAdd = _this.add;
+        // Now create our override
+        _this.add = function (b) {
+            return _this.superAdd(b);
+        };
+        return _this;
+    }
+    return ExtendedAdder;
+}(Adder));
--- Ejercicios Libro 2/fatArrow.ts
+++ Ejercicios Libro 2/fatArrow.ts
@@ -31,10 +31,25 @@ class Person{
 var person = new Person(1);
 class Person {
     constructor(public age:number) {}
-    growOld = () => {
-    this.age++;
+    growOld = () => {this.age++;
+}
+}
+var person = new Person(1);
+setTimeout(person.growOld,1000);
+setTimeout(function() { console.log(person.age); },2000);
+
+    class Adder {
+        // This function is now safe to pass around
+        add = (b: string): string => {
+            return this.a + b;
     }
+}
+
+class ExtendedAdder extends Adder {
+    // Create a copy of parent before creating our own
+    private superAdd = this.add;
+    // Now create our override
+    add = (b: string): string => {
+        return this.superAdd(b);
     }
-    var person = new Person(1);
-    setTimeout(person.growOld,1000);
-    setTimeout(function() { console.log(person.age); },2000);
+}
