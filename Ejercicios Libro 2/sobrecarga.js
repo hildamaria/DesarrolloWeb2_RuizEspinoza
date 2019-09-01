d120448035a2477cbe6c4f065bf7407b8a916458
hildamaria <hildamaria0609@hotmail.com>
2019-09-01 09:24:49 -0600

    Sobrecarga

---
+++ Ejercicios Libro 2/sobrecarga.js
@@ -0,0 +1,4 @@
+function a(x) {
+    //implementacion de la funcion
+}
+//Este tipo de sobrecarga no tiene mucho sentido porque sería más simple poner un Any.
---
+++ Ejercicios Libro 2/sobrecarga.ts
@@ -0,0 +1,8 @@
+function a(x: number);
+function a(x: String);
+function a(x: boolean);
+function a(x: Array<number>);
+function a(x){
+    //implementacion de la funcion
+}
+//Este tipo de sobrecarga no tiene mucho sentido porque sería más simple poner un Any.
