3ce2e77835df5dadd867b57e93bad58d44091f73
hildamaria <hildamaria0609@hotmail.com>
2019-09-01 10:56:46 -0600

    Mixin

---
+++ Ejercicios Libro 2/mixIn.js
@@ -0,0 +1,53 @@
+var Disposable = /** @class */ (function () {
+    function Disposable() {
+    }
+    Disposable.prototype.dispose = function () {
+        this.isDisposed = true;
+    };
+    return Disposable;
+}());
+//Activatable Mixin
+var Activatable = /** @class */ (function () {
+    function Activatable() {
+    }
+    Activatable.prototype.activate = function () {
+        this.isActive = true;
+    };
+    Activatable.prototype.deactivate = function () {
+        this.isActive = false;
+    };
+    return Activatable;
+}());
+var SmartObject = /** @class */ (function () {
+    // Este constructos lo que hará será que mostrará por pantalla
+    // los estados isActive y isDisposed cada 500 ms
+    function SmartObject() {
+        var _this = this;
+        //Disposable
+        this.isDisposed = false;
+        //Activatable
+        this.isActive = false;
+        setInterval(function () { return console.log(_this.isActive + " : " + _this.isDisposed); }, 500);
+    }
+    SmartObject.prototype.interact = function () {
+        this.activate();
+    };
+    return SmartObject;
+}());
+applyMixins(SmartObject, [Disposable, Activatable]);
+var smartobj = new SmartObject();
+// esto generará una interactuación cada segundo cambiando el estado de
+// `false - false` a `true - true` la primera vez,
+// luego como la variable ya valdría `true - true`
+// simplemente se dedicaría a mostrarla cada segundo
+setTimeout(function () { return smartobj.interact(); }, 1000);
+////////////////////////////////////////
+// In your runtime library somewhere
+////////////////////////////////////////
+function applyMixins(derivedCtor, baseCtors) {
+    baseCtors.forEach(function (baseCtor) {
+        Object.getOwnPropertyNames(baseCtor.prototype).forEach(function (name) {
+            derivedCtor.prototype[name] = baseCtor.prototype[name];
+        });
+    });
+}
