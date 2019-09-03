e6627d104684f64a9687a8e7eaed3d440ccb1a1f
hildamaria <hildamaria0609@hotmail.com>
2019-09-02 22:50:45 -0600

    Modulos de Archivos

--- Ejercicios Libro 2/a.ts
+++ Ejercicios Libro 2/a.ts
@@ -1,1 +1,10 @@
 var a = 123;
+
+export var a = 123;
+
+// Se puede realizar un módulo sin emplear declare
+declare module "a" {
+    // Contenido de la clase
+    export var a:number; /*sample*/
+}
+declare var hello = 'Hello world';
--- Ejercicios Libro 2/b.ts
+++ Ejercicios Libro 2/b.ts
@@ -1,1 +1,9 @@
 var b = a; // Permitido
+
+var b = a; // NO permitido - no encontraria el valor de a
+
+import {a} from "./a";
+var b = a; // Permitido, ahora si encontraria el valor
+
+import var a = require("./a");
+var b = a;
